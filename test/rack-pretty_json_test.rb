26bdc9ea61e3284edd1fc3d50bf979659ec1a3eb
Thibaut Barrère <thibaut.barrere@gmail.com>
2011-08-20 18:17:21 +0200

    Only pretty-print for application/json and if the user agent matches mozilla something.

--- test/rack-pretty_json_test.rb
+++ test/rack-pretty_json_test.rb
@@ -5,14 +5,53 @@ class RackPrettyJSONTest < Test::Unit::TestCase
   RAW_JSON = "{\"a\":4}"
   PRETTY_JSON = "{\n  \"a\": 4\n}"
 
-  context "Rack::PrettyJSON" do
-    context "with content type equal to 'application/json'" do
+  CURL_USER_AGENT = "libcurl-agent/1.0"
+  BROWSER_USER_AGENT = "mozilla"
 
+  def do_request(headers)
+    app = lambda { |env| [200, headers, [RAW_JSON]] }
+    request = Rack::MockRequest.env_for("/")
+    
+    Rack::PrettyJSON.new(app).call(request).last.last
+  end
+    
+  context "with application/json content-type" do
+    
+    context "and a non-browser user agent" do
+    
+      should "not pretty print response body" do
+        body = do_request({'Content-Type' => 'application/json', 'User-Agent' => CURL_USER_AGENT})
+        assert_equal RAW_JSON, body
+      end
+
+    end
+    
+    context "and a browser user agent" do
+      
       should "pretty print response body" do
-        app = lambda { |env| [200, {'Content-Type' => 'application/json'}, [RAW_JSON]] }
-        request = Rack::MockRequest.env_for("/")
-        body = Rack::PrettyJSON.new(app).call(request).last
-        assert_equal PRETTY_JSON, body.last
+        body = do_request({'Content-Type' => 'application/json', 'User-Agent' => BROWSER_USER_AGENT})
+        assert_equal PRETTY_JSON, body
+      end
+      
+    end
+  end
+  
+  context "with another content-type" do
+
+    context "with a non-browser user agent" do
+      
+      should "not pretty print response body" do
+        body = do_request({'Content-Type' => 'text/html', 'User-Agent' => CURL_USER_AGENT})
+        assert_equal RAW_JSON, body
+      end
+
+    end
+    
+    context "with a browser user agent" do
+
+      should "not pretty print response body" do
+        body = do_request({'Content-Type' => 'text/html', 'User-Agent' => BROWSER_USER_AGENT})
+        assert_equal RAW_JSON, body
       end
 
     end
